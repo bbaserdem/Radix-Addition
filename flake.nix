c493d500ffcbb0d05728ba48dd4081d4c474eab7
bbaserdem <baserdemb@gmail.com>
2025-02-02 22:15:23 -0600

    UV migration
    Migrated to UV

--- flake.nix
+++ flake.nix
@@ -1,44 +1,108 @@
 # file: flake.nix
 {
-  description = "Python application packaged using poetry2nix";
+  description = "Python application packaged using uv2nix";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
-    flake-utils.url = "github:numtide/flake-utils";
-    poetry2nix = {
-      url = "github:nix-community/poetry2nix";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+
+    pyproject-nix = {
+      url = "github:pyproject-nix/pyproject.nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    uv2nix = {
+      url = "github:pyproject-nix/uv2nix";
+      inputs.pyproject-nix.follows = "pyproject-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    pyproject-build-systems = {
+      url = "github:pyproject-nix/build-system-pkgs";
+      inputs.pyproject-nix.follows = "pyproject-nix";
+      inputs.uv2nix.follows = "uv2nix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = { self, nixpkgs, flake-utils, poetry2nix }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = nixpkgs.legacyPackages.${system};
-        inherit (poetry2nix.lib.mkPoetry2Nix { inherit pkgs; }) mkPoetryApplication;
-      in {
-        packages = {
-          myapp = mkPoetryApplication { projectDir = self; };
-          default = self.packages.${system}.myapp;
-        };
+  outputs = {
+    self,
+    nixpkgs,
+    uv2nix,
+    pyproject-nix,
+    pyproject-build-systems,
+    ...
+  } @ inputs: let
+    # Get lib functions
+    inherit (nixpkgs) lib;
 
-        # Shell for app dependencies
-        #
-        #   nix develop
-        #
-        # Use this shell for developing your app
-        devShells.default = pkgs.mkShell {
-          inputsFrom = [ self.packages.${system}.myapp ];
-        };
+    # Load a uv workspace from workspace root
+    workspace = uv2nix.lib.workspace.loadWorkspace {workspaceRoot = ./.;};
 
-        # Shell for poetry
-        #
-        #   nix develop .#poetry
-        #
-        # Use this shell for changes to pyproject.toml & poetry.lock
-        devShells.poetry = pkgs.mkShell {
-          packages = [ pkgs.poetry ];
-        };
-      }
-    );
+    # Create package overlay from workspace
+    overlay = workspace.mkPyprojectOverlay {
+      sourcePreference = "wheel";
+    };
+
+    # Extend overlay with build fixups
+    # - https://pyproject-nix.github.io/uv2nix/FAQ.html
+    pyprojectOverrides = _final: _prev: {
+    };
+
+    # Architectures; we are just using x86_64-linux
+    pkgs = nixpkgs.legacyPackages.x86_64-linux;
+
+    # Use python 3.12
+    python = pkgs.python312;
+
+    # Make package set
+    pythonSet =
+      # Use base package set from pyproject.nix builders
+      (pkgs.callPackage pyproject-nix.build.packages {
+        inherit python;
+      })
+      .overrideScope (lib.composeManyExtensions [
+        pyproject-build-systems.overlays.default
+        overlay
+        pyprojectOverrides
+      ]);
+  in {
+    # Formatting for the flake
+    formatter.x86_64-linux = pkgs.alejandra;
+
+    # Package a virtual environment as main application
+    packages.x86_64-linux.default =
+      pythonSet.mkVirtualEnv
+      "radix-addition-env"
+      workspace.deps.default;
+
+    # Make us runnable with nix run
+    apps.x86_64-linux = {
+      default = {
+        type = "app";
+        program = "${self.packages.x86_64-linux.default}/bin/radix-addition";
+      };
+    };
+
+    # The impure shell to do virtualenv workflow
+    devShells.x86_64-linux = {
+      # Undo the dependency leakage by nix
+      impure = pkgs.mkShell {
+        packages = [
+          python
+          pkgs.uv
+        ];
+        env =
+          {
+            UV_PYTHON_DOWNLOADS = "never";
+            UV_PYTHON = python.interpreter;
+          }
+          // lib.optionalAttrs pkgs.stdenv.isLinux {
+            LD_LIBRARY_PATH = lib.makeLibraryPath pkgs.pythonManylinuxPackages.manylinux1;
+          };
+        shellHook = ''
+          unset PYTHONPATH
+        '';
+      };
+    };
+  };
 }
